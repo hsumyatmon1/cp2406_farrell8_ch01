4fd4cc12f0c682e68e3e9cdd9f68c15b0aac0f95
hsumyatmon <hsumyatmon278@gmail.com>
2022-11-23 15:58:37 +0800

    Practical commit

---
+++ Exercises/MovieQuote.java
@@ -0,0 +1,5 @@
+public class MovieQuote {
+    public static void main(String[] args){
+        System.out.println("Set your heart ablaze");
+    }
+}
---
+++ Exercises/MovieQuoteInfo.java
@@ -0,0 +1,6 @@
+public class MovieQuoteInfo
+{
+    public static void main(String[] args){
+        System.out.println("Kennyyyyyyyyyyyyyy - Levi Ackerman (Attack on Titan)");
+    }
+}
